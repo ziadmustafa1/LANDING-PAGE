a86986ef3b75ce236a913cf23e3789d82e94daff
ziadmustafa1 <124303556+ziadmustafa1@users.noreply.github.com>
2023-06-07 18:44:08 +0300

    Add files via upload

--- mian.js
+++ mian.js
@@ -9,3 +9,12 @@ closeButton.addEventListener('click', () => {
   menu.classList.remove('show');
   menuButton.classList.remove('hide');
 });
+ScrollReveal({ reset: true,
+    distance :'80px',
+    duration : 2000,
+    delay : 200
+    });
+    ScrollReveal().reveal('.l, .text-4xl, .text-center ', { origin: 'top' });
+    ScrollReveal().reveal('.text-slate-300, h3 , .card1 , .grid , .container, .svg-wrapper', { origin: 'bottom' });
+    ScrollReveal().reveal('.home-content h1, .about-img', { origin: 'left' });
+    ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
